40fc75b8eee7e7c3307d3ff2a6733f343ced1a8e
Anneta <annetawamono@yahoo.com.au>
2018-02-22 16:15:28 +0200

    Section 1 and 4 mostly completed

--- js/script-parallax.js
+++ js/script-parallax.js
@@ -1,10 +1,13 @@
 $(document).ready(function(){
-  var $el = $('div.row.bbcement-3>div:nth-child(2)');
-   $(window).scroll(function() {
-       var scroll = $(document).scrollTop();
-       console.log(scroll);
-       $el.css({
+  	var $el = $('div.row.bbcement-3>div:nth-child(2)');
+	var $el2 = $('div.row.land-4>div:nth-child(1)');
+   	$(window).scroll(function() {
+       	var scroll = $(document).scrollTop();
+       	$el.css({
            'background-position':'50% '+(-0.15*scroll+290)+'px'
-       });
+       	});
+		$el2.css({
+			'background-position':'50% '+(-0.15*scroll)+'px'
+		});
    });
 });
